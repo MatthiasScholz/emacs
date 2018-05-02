73ff01f83de68bb0b92c551cdbed9bfd32632035
Matthias Scholz <matthias.scholz@gmail.com>
2018-05-03 00:21:29 +0200

    .INIT using spacemacs configuration

---
+++ general/packages.el
@@ -0,0 +1,170 @@
+;;; packages.el --- general layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Matthias Scholz <mat@MatMac.fritz.box>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `general-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `general/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `general/pre-init-PACKAGE' and/or
+;;   `general/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst general-packages
+  '(
+    origami
+    highlight-symbol
+    helm-mt
+    helm-swoop
+    company
+    dockerfile-mode
+    docker-tramp
+    paradox
+    )
+  )
+
+(defun general/post-init-paradox ()
+  (setq paradox-github-token '53f1f0c65f90f8fea51e9ced4ae49f1dd7f5fffd))
+
+(defun general/post-init-origami ()
+  (use-package origami
+    :config
+    (setq global-origami-mode 't)
+  (global-set-key (kbd "TAB") 'origami-recursively-toggle-node)))
+
+(defun general/init-highlight-symbol ()
+  (use-package highlight-symbol
+    :config
+    (setq global-auto-highlight-symbol-mode 't)
+    (setq highlight-symbol-nav-mode 't)
+    (setq highlight-symbol-idle-delay 0.1)))
+
+(defun general/init-helm-mt ()
+  (use-package helm-mt
+              :after helm multi-term))
+
+(defun general/init-dockerfile-mode ()
+  (use-package dockerfile-mode))
+
+(defun general/init-docker-tramp ()
+  (use-package docker-tramp))
+
+
+;; TODO REFACTOR it could make sense to put helm-swoop into a separate layer since lots of configuration is provided here.
+(defun general/post-init-helm-swoop ()
+  (use-package helm-swoop
+    :after helm
+    :config
+    (progn
+      ;; Change the general keybinds to whatever you like :)
+      (global-set-key (kbd "M-i") 'helm-swoop)
+      (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+      (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+      (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
+      ;; When doing isearch, hand the word over to helm-swoop
+      (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+      ;; From helm-swoop to helm-multi-swoop-all
+      (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+      ;; When doing evil-search, hand the word over to helm-swoop
+      ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+
+      ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
+      (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+
+      ;; Move up and down like isearch
+      (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+      (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+      (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
+      (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
+      (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
+      ;; When doing isearch, hand the word over to helm-swoop
+      (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+      ;; From helm-swoop to helm-multi-swoop-all
+      (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+      ;; When doing evil-search, hand the word over to helm-swoop
+      ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+
+      ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
+      (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+
+      ;; Move up and down like isearch
+      (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+      (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
+      (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
+      (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
+
+      ;; ;; If this value is t, split window inside the current window
+      (setq helm-swoop-split-with-multiple-windows nil)
+      ;; ;; Split direction. 'split-window-vertically or 'split-window-horizontally
+      (setq helm-swoop-split-direction 'split-window-horizontally)
+
+      ;; Save buffer when helm-multi-swoop-edit complete
+      (setq helm-multi-swoop-edit-save t)
+
+      ;; If nil, you can slightly boost invoke speed in exchange for text color
+      (setq helm-swoop-speed-or-color t)
+
+      ;; ;; Go to the opposite side of line from the end or beginning of line
+      (setq helm-swoop-move-to-line-cycle t)
+
+      ;; Optional face for line numbers
+      ;; Face name is `helm-swoop-line-number-face`
+      (setq helm-swoop-use-line-number-face t)
+
+      ;; If you prefer fuzzy matching
+      (setq helm-swoop-use-fuzzy-match t)
+
+      ;; If there is no symbol at the cursor, use the last used words instead.
+      (setq helm-swoop-pre-input-function
+            (lambda ()
+              (let (($pre-input (thing-at-point 'symbol)))
+                (if (eq (length $pre-input) 0)
+                    helm-swoop-pattern ;; this variable keeps the last used words
+                  $pre-input))))
+    )
+  )
+)
+
+;; generic company configuration
+(defun general/post-init-company ()
+  (setq company-tooltip-limit 20)                      ; bigger popup window
+  (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+  (setq company-echo-delay 0)                          ; remove annoying blinking
+  (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+)
+
+
+;; Keybindings
+(global-set-key (kbd "M-s o") 'helm-occur)
+
+;; Neo2 configuration:
+(setq ns-right-alternate-modifier nil)
+(setq ns-alternate-modifier 'meta)
+;; Deactivte special space of the layer 3
+(global-set-key (kbd " ") " ")
+
+;; Overwrite selection with pasted text
+(delete-selection-mode 1)
+
+;;; packages.el ends here
---
+++ golang/packages.el
@@ -0,0 +1,62 @@
+;;; packages.el --- golang layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Matthias Scholz <mat@MatMac.fritz.box>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `golang-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `golang/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `golang/pre-init-PACKAGE' and/or
+;;   `golang/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst golang-packages
+  '(
+    projectile
+    go-projectile
+    company-go
+    )
+  )
+
+(defun golang/post-init-projectile ()
+  (when (memq window-system '(mac ns))
+     (exec-path-from-shell-initialize)
+     (exec-path-from-shell-copy-env "GOPATH"))
+  )
+
+(defun golang/init-go-projectile ()
+  (use-package go-projectile
+    ))
+
+(defun golang/init-company-go ()
+  (use-package company-go
+    ;; This has to be in init because it's a package entry point
+    :init
+    (progn
+      (add-hook 'go-mode-hook (lambda ()
+                                (set (make-local-variable 'company-backends) '(company-go))
+                                (company-mode))))
+    ))
+
+;;; packages.el ends here
+
+    
