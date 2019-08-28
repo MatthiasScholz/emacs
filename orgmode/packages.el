50232b4b1d97c503d5658279ab4859d9d62caee4
Matthias Scholz <matthias@Matthiass-MacBook-Pro.local>
2019-08-28 19:18:02 +0200

    .NEW daily custom agenda view - shortcut "d"
    Providing a new custom agenda view focused on the day. Structure:
    1. High priority tasks - labled #A
    2. Scheduled tasks
    3. Unscheduled tasks

--- orgmode/packages.el
+++ orgmode/packages.el
@@ -94,6 +94,47 @@
           ))
   )
 
+;; Setup a custom agenda view
+(defun help-org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t)))
+        )
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)
+    )
+  )
+
+;; Filtering support for habits
+(defun help-org-skip-subtree-if-habit ()
+  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (string= (org-entry-get nil "STYLE") "habit")
+        subtree-end
+      nil)))
+
+;; Setting up a custom agenda view. Listing:
+;; .high priority tasks ( "A" )
+;; .scheduled tasks for the current day
+;; .unscheduled tasks
+;; HINT: Take into account that the default priority of tasks is "B"
+(setq org-agenda-custom-commands
+      '(("d" "Daily agenda and all TODOs"
+         ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'TODO 'DONE 'CANCELED 'DELEGATED))
+                 (org-agenda-overriding-header "High-priority unfinished tasks:")))
+          (agenda "" ((org-agenda-span 1)))
+          (alltodo ""
+                   ((org-agenda-skip-function '(or (help-org-skip-subtree-if-habit)
+                                                   (help-org-skip-subtree-if-priority ?A)
+                                                   (org-agenda-skip-if nil '(scheduled deadline))))
+                    (org-agenda-overriding-header "ALL normal priority tasks:"))))
+         )))
+
 (defun orgmode/post-init-org-inlinetask ()
   )
 
