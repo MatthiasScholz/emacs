8a197bbaec04d37681aa6defff214ea1bb9c51a3
Matthias Scholz <matthias.scholz@thoughtworks.com>
2020-06-25 18:58:19 +0200

    :recycle: helm-swoop already built into spacemacs

--- general/packages.el
+++ general/packages.el
@@ -33,7 +33,6 @@
   '(
     highlight-symbol
     helm-mt
-    helm-swoop
     company
     paradox
     hcl-mode
@@ -75,82 +74,6 @@
   (add-to-list 'company-backends 'company-restclient)
   )
 
-;; TODO REFACTOR it could make sense to put helm-swoop into a separate layer since lots of configuration is provided here.
-(defun general/post-init-helm-swoop ()
-  (use-package helm-swoop
-    :after helm
-    :config
-    (progn
-      ;; Change the general keybinds to whatever you like :)
-      (global-set-key (kbd "M-i") 'helm-swoop)
-      (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
-      (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
-      (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
-
-      ;; When doing isearch, hand the word over to helm-swoop
-      (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-      ;; From helm-swoop to helm-multi-swoop-all
-      (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-      ;; When doing evil-search, hand the word over to helm-swoop
-      ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
-
-      ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
-      (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
-
-      ;; Move up and down like isearch
-      (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-      (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-      (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
-      (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
-      (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
-
-      ;; When doing isearch, hand the word over to helm-swoop
-      (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-      ;; From helm-swoop to helm-multi-swoop-all
-      (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-      ;; When doing evil-search, hand the word over to helm-swoop
-      ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
-
-      ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
-      (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
-
-      ;; Move up and down like isearch
-      (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
-      (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-      (define-key helm-multi-swoop-map (kbd "C-r") 'helm-previous-line)
-      (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
-
-      ;; ;; If this value is t, split window inside the current window
-      (setq helm-swoop-split-with-multiple-windows nil)
-      ;; ;; Split direction. 'split-window-vertically or 'split-window-horizontally
-      (setq helm-swoop-split-direction 'split-window-horizontally)
-
-      ;; Save buffer when helm-multi-swoop-edit complete
-      (setq helm-multi-swoop-edit-save t)
-
-      ;; If nil, you can slightly boost invoke speed in exchange for text color
-      (setq helm-swoop-speed-or-color t)
-
-      ;; ;; Go to the opposite side of line from the end or beginning of line
-      (setq helm-swoop-move-to-line-cycle t)
-
-      ;; Optional face for line numbers
-      ;; Face name is `helm-swoop-line-number-face`
-      (setq helm-swoop-use-line-number-face t)
-
-      ;; If you prefer fuzzy matching
-      (setq helm-swoop-use-fuzzy-match t)
-
-      ;; If there is no symbol at the cursor, use the last used words instead.
-      (setq helm-swoop-pre-input-function
-            (lambda ()
-              (let (($pre-input (thing-at-point 'symbol)))
-                (if (eq (length $pre-input) 0)
-                    helm-swoop-pattern ;; this variable keeps the last used words
-                  $pre-input))))
-    )
-  )
-)
 
 ;; generic company configuration
 (defun general/post-init-company ()
